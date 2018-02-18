60bce160f4cf5dbda68302a33bbe0d1a4e378876
Ben Hilburn <bhilburn@gmail.com>
2018-02-17 22:22:51 -0500

    Changing CAPS LOCK to LCTRL

--- keyboards/dz60/keymaps/cstd60/keymap.c
+++ keyboards/dz60/keymaps/cstd60/keymap.c
@@ -6,13 +6,13 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /* Layer 0 - QWERTY, standard 60% without arrows, Space Cadet Shifts
+  /* Layer 0 - QWERTY, standard 60% without arrows, Space Cadet Shifts, 'Linux Ctrl'
   * ,-----------------------------------------------------------------------------------------.
   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |   Bkspc   |
   * |-----------------------------------------------------------------------------------------+
   * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |    \   |
   * |-----------------------------------------------------------------------------------------+
-  * | Caps    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
+  * | LCtrl   |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
   * |-----------------------------------------------------------------------------------------+
   * | Shift    |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |    Right Shift   |
   * |-----------------------------------------------------------------------------------------+
@@ -22,17 +22,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(
       F(0),     KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,      KC_8,   KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_NO,    KC_BSPC,
       KC_TAB,   KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,      KC_I,   KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
-      KC_CAPS,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,     KC_J,      KC_K,   KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
+      KC_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,    KC_G,    KC_H,     KC_J,      KC_K,   KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,
       KC_LSPO,  KC_NO,    KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_N,      KC_M,   KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSPC,  KC_RSPC,
       KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT,  KC_RCTRL,  KC_NO,  MO(2),    MO(3)),
 
-  /* Layer 1 - Dvorak, standard 60% without arrows, Space Cadet Shifts
+  /* Layer 1 - Dvorak, standard 60% without arrows, Space Cadet Shifts, 'Linux Ctrl'
   * ,-----------------------------------------------------------------------------------------.
   * | ` ~ |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  [  |  ]  |   Bkspc   |
   * |-----------------------------------------------------------------------------------------+
   * | Tab    |  "  |  <  |  >  |  P  |  Y  |  F  |  G  |  C  |  R  |  L  |  /  |  =  |    \   |
   * |-----------------------------------------------------------------------------------------+
-  * | Caps    |  A  |  O  |  E  |  U  |  I  |  D  |  H  |  T  |  N  |  S  |  -  |    Enter    |
+  * | LCtrl   |  A  |  O  |  E  |  U  |  I  |  D  |  H  |  T  |  N  |  S  |  -  |    Enter    |
   * |-----------------------------------------------------------------------------------------+
   * | Shift    |  :  |  Q  |  J  |  K  |  X  |  B  |  M  |  W  |  V  |  Z  |    Right Shift   |
   * |-----------------------------------------------------------------------------------------+
@@ -40,11 +40,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `-----------------------------------------------------------------------------------------'
   */
   KEYMAP(
-      F(0),     KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,    KC_7,     KC_8,      KC_9,   KC_0,    KC_LBRC,  KC_RBRC,  KC_NO,    KC_BSPC,
-      KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,     KC_C,      KC_R,   KC_L,    KC_SLSH,  KC_EQL,   KC_BSLS,
-      KC_CAPS,  KC_A,     KC_O,     KC_E,    KC_U,    KC_I,    KC_D,    KC_H,     KC_T,      KC_N,   KC_S,    KC_MINS,  KC_ENT,
-      KC_LSPO,  KC_NO,    KC_SCLN,  KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,     KC_M,      KC_W,   KC_V,    KC_Z,     KC_RSPC,  KC_RSPC,
-      KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT,  KC_RCTRL,  KC_NO,  MO(2),    MO(3)),
+      F(0),     KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,      KC_8,   KC_9,   KC_0,    KC_LBRC,  KC_RBRC,  KC_NO,    KC_BSPC,
+      KC_TAB,   KC_QUOT,  KC_COMM,  KC_DOT,  KC_P,    KC_Y,    KC_F,     KC_G,      KC_C,   KC_R,   KC_L,    KC_SLSH,  KC_EQL,   KC_BSLS,
+      KC_LCTL,  KC_A,     KC_O,     KC_E,    KC_U,    KC_I,    KC_D,     KC_H,      KC_T,   KC_N,   KC_S,    KC_MINS,  KC_ENT,
+      KC_LSPO,  KC_NO,    KC_SCLN,  KC_Q,    KC_J,    KC_K,    KC_X,     KC_B,      KC_M,   KC_W,   KC_V,    KC_Z,     KC_RSPC,  KC_RSPC,
+      KC_LCTL,  KC_LGUI,  KC_LALT,  KC_SPC,  KC_SPC,  KC_SPC,  KC_RALT,  KC_RCTRL,  KC_NO,  MO(2),  MO(3)),
 
   /* Layer 2 - Function Layer 1
   * ,-----------------------------------------------------------------------------------------.
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |-----------------------------------------------------------------------------------------+
   * |        |     |  UP |    | PGUP | HME | PSCR|     |     |     |     |     |     |  INS   |
   * |-----------------------------------------------------------------------------------------+
-  * |         |  LT |  DN |  RT | PGDN|  END | PAUS|     |     |     |     |     |            |
+  * |  Caps   |  LT |  DN |  RT | PGDN|  END | PAUS|     |     |     |     |     |            |
   * |-----------------------------------------------------------------------------------------+
   * |          |     |     |     |     |     |     |     |     |     |     |                  |
   * |-----------------------------------------------------------------------------------------+
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(
       KC_GRV,  KC_F1,    KC_F2,    KC_F3,     KC_F4,    KC_F5,    KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  ______,  KC_DEL,
       ______,  ______,   KC_UP,    ______,    KC_PGUP,  KC_HOME,  KC_PSCR,  ______,  ______,  ______,  ______,   ______,  ______,  KC_INS,
-      ______,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_PGDN,  KC_END,   KC_PAUS,  ______,  ______,  ______,  ______,   ______,  ______,
+      KC_CAPS, KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_PGDN,  KC_END,   KC_PAUS,  ______,  ______,  ______,  ______,   ______,  ______,
       ______,  ______,   ______,   ______,    ______,   ______,   ______,   ______,  ______,  ______,  ______,   ______,  ______,  ______,
       ______,  ______,   ______,   ______,    ______,   ______,   ______,   ______,  ______,  ______,  ______),
 
@@ -72,8 +72,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MSTP,  KC_MPLY,  ______,   ______,   ______,   ______,  ______,   ______,  ______,  ______,  ______,
       ______,  RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  ______,  ______,   ______,  ______,  ______,
       ______,  BL_TOGG,  BL_STEP,  BL_DEC,   BL_INC,   ______,   ______,   ______,   ______,   ______,  ______,   ______,  ______,
-      ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  TO(0),   TO(1),    ______,  ______,  ______,
-      RESET,   ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______,  ______),
+      ______,  ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   TO(0),   TO(1),    ______,  ______,  ______,
+      RESET,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,   ______,  ______),
 };
 
 enum function_id {
